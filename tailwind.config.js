08d177f773513e6dc64d3d7b10e66652bb39512f
Mostafijur-Rahman <mosta5617@gmail.com>
2023-07-24 10:56:15 +0600

    github navbar design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "githubNav" : "#dfdfdf",
+        "githubName" : "#161B22",
+      }
+    },
   },
   plugins: [],
 }
